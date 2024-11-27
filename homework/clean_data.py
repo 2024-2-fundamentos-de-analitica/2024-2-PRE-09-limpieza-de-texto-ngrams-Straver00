c653ee41dd6fe0622889ba0d2e6cccbf52e435d5
Straver00 <macuino.555@gmail.com>
2024-11-27 10:52:39 -0500

    update test

--- homework/clean_data.py
+++ homework/clean_data.py
@@ -75,4 +75,4 @@ def main(input_file, output_file, n=2):
 
 
 if __name__ == "__main__":
-    main(input_file="files\input\input.txt", output_file="files/output/output.txt")
+    main(input_file="files\input\input.txt", output_file="files/output.txt")
